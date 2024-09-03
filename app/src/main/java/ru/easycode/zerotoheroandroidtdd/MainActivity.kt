0f8fffd3f04b7b9864d9ac62283ecd846d145985
Pavel Anhur <pavelanhur@gmail.com>
2024-09-03 14:17:44 +0300

    keep text

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,14 +7,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.changeButton)
-        val textView: TextView = findViewById(R.id.titleTextView)
+        textView = findViewById(R.id.titleTextView)
         button.setOnClickListener {
             textView.text = "I am an Android Developer!"
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val textToSave = textView.text.toString()
+        outState.putString("textTitleView", textToSave)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("textTitleView")
     }
 }
