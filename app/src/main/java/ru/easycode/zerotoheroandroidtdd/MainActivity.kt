4dabc3f9b8b590e6f62b749e54772a01fe14447d
Pavel Anhur <pavelanhur@gmail.com>
2024-09-03 12:09:37 +0300

    change text in runtime

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.changeButton)
+        val textView: TextView = findViewById(R.id.titleTextView)
+        button.setOnClickListener {
+            textView.text = "I am an Android Developer!"
+        }
     }
 }
