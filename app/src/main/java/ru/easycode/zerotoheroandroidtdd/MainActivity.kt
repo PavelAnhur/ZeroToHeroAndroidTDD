b9cda89b414c337567293461901bb548fb6ea4a4
Pavel Anhur <pavelanhur@gmail.com>
2024-09-04 12:11:14 +0300

    remove text view

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,39 +2,39 @@ package ru.easycode.zerotoheroandroidtdd
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var textView: TextView
+    private lateinit var rootLayout: LinearLayout
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.changeButton)
+        val removeButton: Button = findViewById(R.id.removeButton)
         textView = findViewById(R.id.titleTextView)
-        button.setOnClickListener {
-            textView.text = "I am an Android Developer!"
-        }
-        val hideButton: Button = findViewById(R.id.hideButton)
-        hideButton.setOnClickListener {
-            textView.visibility = View.INVISIBLE
+        rootLayout = findViewById(R.id.rootLayout)
+        removeButton.setOnClickListener {
+            rootLayout.removeView(textView)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        val textToSave = textView.text.toString()
-        outState.putString("textTitleView", textToSave)
-        outState.putInt("textViewVisibility", textView.visibility)
+        outState.putInt("rootLayoutChildCount", rootLayout.childCount)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        textView.text = savedInstanceState.getString("textTitleView")
-        textView.visibility = savedInstanceState.getInt("textViewVisibility")
+        val childCount = rootLayout.childCount
+        val rootLayoutChildCount = savedInstanceState.getInt("rootLayoutChildCount")
+        if (childCount != rootLayoutChildCount) {
+            rootLayout.removeView(textView)
+        }
     }
 }
