02151b4fb3018038f08bcf36526ebd25a1c714b6
Pavel Anhur <pavelanhur@gmail.com>
2024-09-04 12:11:14 +0300

    remove text view

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var rootLayout: LinearLayout
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val removeButton: Button = findViewById(R.id.removeButton)
+        textView = findViewById(R.id.titleTextView)
+        rootLayout = findViewById(R.id.rootLayout)
+        removeButton.setOnClickListener {
+            rootLayout.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("rootLayoutChildCount", rootLayout.childCount)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val childCount = rootLayout.childCount
+        val rootLayoutChildCount = savedInstanceState.getInt("rootLayoutChildCount")
+        if (childCount != rootLayoutChildCount) {
+            rootLayout.removeView(textView)
+        }
     }
 }
